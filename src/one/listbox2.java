42591780f0dad03afb7006937aa1ee116c4c72eb
SaMiRaN <SaMiRaN@DESKTOP-47DQ67K>
2019-11-11 22:29:03 +0530

    87u9h

---
+++ src/one/listbox2.java
@@ -0,0 +1,27 @@
+package one;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class listbox2 {
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Radhika\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");		
+		   WebDriver driver= new ChromeDriver();
+		   driver.findElement(by.[//select [@aria-label='Day']]);
+		                          
+		  
+		   
+		
+		
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
